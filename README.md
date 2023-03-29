87b11b94cf2bfc18a87f5181d0b13003f4de2291
Gaspare <Gas9220@users.noreply.github.com>
2023-03-29 15:08:54 +0200

    Project Setup

---
+++ README.md
@@ -0,0 +1,8 @@
+# Vue 3 + Vite + SASS
+
+Applicazione creata usando VueJS 3, Vite e SASS
+
+## Descrizione
+In questa applicazione è possibile cercare un film/serie TV e avere informazioni su di esso.
+
+
---
+++ src/main.js
@@ -0,0 +1,11 @@
+// Vue app creation
+import { createApp } from 'vue'
+
+// Main style
+import './assets/scss/main.scss';
+
+// Main App component import
+import App from './App.vue'
+
+// App mounted
+createApp(App).mount('#app')
