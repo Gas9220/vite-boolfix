b5764dc69588a8d5f797e902b7dd5953e4b7e17b
Gaspare <Gas9220@users.noreply.github.com>
2023-03-29 17:19:56 +0200

    Store creation and API test for movies and TvShow

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,44 @@
-<script setup>
+<script>
+import { store } from './store';
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      store
+    }
+  },
+  methods: {
+    // Test API
+    testAPI(baseURL, query) {
+      const request = baseURL
+
+      axios.get(request, {
+        params: {
+          api_key: store.apiKey,
+          language: store.language,
+          query: query
+        }
+      })
+        .then(function (response) {
+          console.log(request)
+          console.log("Response", response.data.results);
+        })
+        .catch(function (error) {
+          console.log("Error", error.data.results);
+        })
+    },
+    // Test API with movie and tv show
+    performAPITest() {
+      this.testAPI(this.store.baseURLTv, 'Lord of the rings')
+      this.testAPI(this.store.baseURLTv, 'Prison Break')
+    }
+  },
+  mounted() {
+    this.performAPITest()
+  }
+}
 </script>
 
 <template>
---
+++ src/store.js
@@ -0,0 +1,21 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+    // Text from the film/tvShow input
+    searchQuery: '',
+    // Array that contains all tvShows that match with searchQuery
+    tvShows: [],
+    // Array that contains all tvShows that match with searchQuery
+    films: [],
+    // Base URL for search movies
+    baseURLMovies: 'https://api.themoviedb.org/3/search/movie?',
+    // Base URL for search tv shows
+    baseURLTv: 'https://api.themoviedb.org/3/search/tv?',
+    // API key from https://www.themoviedb.org/
+    apiKey: 'c19cace3f5571244b4a9b08323eb49f9',
+    // Language of the request
+    language: 'it'
+})
+
+
+//https://api.themoviedb.org/3/search/tv?api_key=c19cace3f5571244b4a9b08323eb49f9&query=prison
